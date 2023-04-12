e752583830982a191bcdd32242f2b511414aa5f1
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-04-13 00:04:20 +1000

    Cleanup of unused code

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -53,22 +53,6 @@ function App() {
 			<div className="layout">
 
 				<aside>
-					{/* <div className='genre-filters'>
-						<h3> Genres </h3>
-						<div>
-							<div className='pane'>
-							{
-								activeGenres.map( (genre, index) => <button onClick={()=>deactivateGenre(index)} className='toggled'> {genre} </button>)
-							}
-							</div>
-							<div className='pane'>
-							{
-								inactiveGenres.map( (genre, index) => <button onClick={()=>activateGenre(index)} className='untoggled'> {genre} </button>)
-							}
-							</div>
-						</div>
-					</div> */}
-
 					<div className='news'>
 						<h3> News </h3>
 						<div className='pane'>
@@ -98,7 +82,17 @@ function App() {
 							<div className="anime-card">
 								<img className="anime-poster" src="https://api-cdn.myanimelist.net/images/anime/1160/122627l.jpg" alt="" />
 							<div className="anime-card-content">
-								<div className="play-button"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1rem" width="1rem" xmlns="http://www.w3.org/2000/svg"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg></div><h4 className="title"> Kaguya-sama: Love is War - Ultra Romantic </h4><span className="rating"> <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg> 9.06 </span>
+								<div className="play-button">
+									<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1rem" width="1rem" xmlns="http://www.w3.org/2000/svg">
+										<path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+									</svg>
+								</div>
+								<h4 className="title"> Kaguya-sama: Love is War - Ultra Romantic </h4>
+								<span className="rating"> 
+									<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+										<path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+									</svg> 9.06 
+								</span>
 								</div>
 							</div>
 						</div>
@@ -106,7 +100,7 @@ function App() {
 							<div className="anime-card">
 								<img className="anime-poster" src="https://api-cdn.myanimelist.net/images/anime/1279/131078l.jpg" alt="" />
 							<div className="anime-card-content">
-								<div className="play-button"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1rem" width="1rem" xmlns="http://www.w3.org/2000/svg"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg></div><h4 className="title"> Attack on Titan: Final Season - The Final Chapters </h4><span className="rating"> <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg> 9.12 </span>
+								<div style={{color: "white"}} className="play-button"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1rem" width="1rem" xmlns="http://www.w3.org/2000/svg"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg></div><h4 className="title"> Attack on Titan: Final Season - The Final Chapters </h4><span className="rating"> <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg> 9.12 </span>
 								</div>
 							</div>
 						</div>
@@ -136,52 +130,36 @@ function App() {
 export default App
 
 const SectionHeader = ({heading}: {heading: string}) => {
-
 	return (
 		<div className='section-header'>
 			<h2> {heading} </h2>
 			<a> See More... </a>
 		</div>
 	)
-
 }
 
-const AnimeCard = ({anime, ref}: {anime: Anime, ref?: any}) => {	
+const AnimeCard = ({anime}: {anime: Anime}) => {	
 	return (
 		<Card>
 			<div className='anime-card'>
 				<img className='anime-poster' src={anime.main_picture.large} alt="" />	
 				<div className='anime-card-content'>
-					<a href={`${anime.id}`} className='play-button'><FaPlay size={"1rem"}/></a>
+					<a style={{color: "white"}} href={`${anime.id}`} className='play-button'><FaPlay size={"1rem"}/></a>
 					<h4 className='title'> {!anime.alternative_titles.en ? anime.title : anime.alternative_titles.en} </h4>
 					<span className='rating'> <FaStar size={20} /> {anime.mean} </span>
 				</div>
 			</div>
 		</Card>
-)}
+	)
+}
 
 const SeasonalAnime = () => {
 	const today = new Date();
 	const seasonalAnime = useAnime( `http://localhost:8080/${today.getFullYear()}/${getSeason(today.getMonth()+1)}?preview=true` );
-
-	return (
-		<>
-			{
-				seasonalAnime.map( (anime: Anime) => <AnimeCard anime={anime} key={'seasonal-'+anime.id} /> )
-			}
-		</>
-	)
+	return seasonalAnime.map( (anime: Anime) => <AnimeCard anime={anime} key={'seasonal-'+anime.id} /> )
 }
 
 const TopAnime = () => {
 	const topAnime = sortBy(useAnime( `http://localhost:8080/anime/top?preview=true` ), 'rank', 'desc');
-	return (
-		<>
-		{
-			topAnime.map( (anime: Anime) => <AnimeCard anime={anime} key={anime.id} /> )
-		}
-		</>
-	)
-	
-
+	return topAnime.map( (anime: Anime) => <AnimeCard anime={anime} key={anime.id} /> )
 }
