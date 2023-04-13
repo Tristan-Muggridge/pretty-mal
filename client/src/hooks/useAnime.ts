9d19f9429946469f9a5d07f6b57660a95ca3c840
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-04-13 18:17:18 +1000

    Update useAnime.ts

--- client/src/hooks/useAnime.ts
+++ client/src/hooks/useAnime.ts
@@ -1,50 +1,51 @@
 import { useEffect, useState } from "react";
 import Anime from "../types/Anime";
-import IMALSearchResult, { IMALAnime } from "../types/IMALSearchResult";
-import { getSeason } from "../types/Seasons";
+import { IMALAnime } from "../types/IMALSearchResult";
 
 const headers = {'Content-Type': 'application/json'};
+
+const readFromLocalStorage = (url: string):Anime[] => {
+    const anime = localStorage.getItem(url);
+    if (!anime) return [];
+
+    const {anime: localAnime, TTL} = JSON.parse(anime);
+    if (TTL < Date.now()) return [];
+
+    console.debug('Hours Remaining on TTL:', Math.floor((TTL - Date.now()) / 1000 / 60 / 60));
+    return localAnime;
+}
+
+const writeToLocalStorage = (url: string, anime: Anime[]) => {
+    const TTLDuration = 60 * 60 * 24; // 24 hours
+    localStorage.setItem(url, JSON.stringify({anime, TTL: Date.now() + TTLDuration * 1000}));
+}
 
 export default function useAnime(url: string, body: object | undefined = undefined) {
     const [anime, setAnime] = useState<Anime[]>([]);
 
-    useEffect(() => {
-        const currentSeason = getSeason(new Date().getMonth());
-        
+    useEffect(() => {       
         const search = async () => {
             
             // check if the anime is in local storage
-            const animeFromLocalStorage = localStorage.getItem(url);
-            if (animeFromLocalStorage) {
-                console.debug('Anime found in local storage')
-                console.debug('expires in ' + (JSON.parse(animeFromLocalStorage).TTL - Date.now()) + 'ms')
-                const {anime, TTL} = JSON.parse(animeFromLocalStorage);
-                if (TTL > Date.now()) {
-                    setAnime(anime);
-                    return;
-                }
+            const animeFromLocalStorage = readFromLocalStorage(url);
+            if (animeFromLocalStorage.length > 0) {
+                setAnime(animeFromLocalStorage);
+                return;
             }
             
-            console.debug('Anime not found in local storage');
             const request = await fetch(url, {
                 method: body ? 'post' : 'get', 
                 body: JSON.stringify(body), 
                 headers: headers
-            })
+            });
 
             const json = await request.json();
-            const {data, error} = json;
-            
-            if (error) {
-                console.error(error);
-                return;
-            }
+            const {data, error} = json;    
 
-            console.debug(data, url)
             const animeArray = data.map( (result: {node: IMALAnime}) => new Anime(result.node));
 
-            // create a TTL for the anime set to 1 minute from now
-            localStorage.setItem(url, JSON.stringify({anime: animeArray, TTL: Date.now() + 60000}));
+            writeToLocalStorage(url, animeArray);
+
             setAnime(animeArray)
         }
 
